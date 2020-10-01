949e6bbc4b2b202e9f57f7af46cf2d54dafcac95
Saint <saint@watson.local>
2020-10-01 10:36:52 +0300

    Complete SwaggerUrMiddleware test

--- tests/SwaggerUiTest.php
+++ tests/SwaggerUiTest.php
@@ -9,40 +9,40 @@ use Nyholm\Psr7\Response;
 use Nyholm\Psr7\ServerRequest;
 use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerInterface;
-use Psr\EventDispatcher\EventDispatcherInterface;
-use Psr\EventDispatcher\ListenerProviderInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Psr\Log\LoggerInterface;
 use Psr\SimpleCache\CacheInterface;
 use Yiisoft\Aliases\Aliases;
 use Yiisoft\Cache\ArrayCache;
+use Yiisoft\Csrf\CsrfToken;
 use Yiisoft\DataResponse\DataResponseFactory;
 use Yiisoft\DataResponse\DataResponseFactoryInterface;
 use Yiisoft\Di\Container;
-use Yiisoft\EventDispatcher\Dispatcher\Dispatcher;
-use Yiisoft\EventDispatcher\Provider\Provider;
 use Yiisoft\Http\Method;
-use Yiisoft\Log\Logger;
 use Yiisoft\Swagger\Middleware\SwaggerUi;
 use Yiisoft\Swagger\Service\SwaggerService;
-use Yiisoft\Yii\View\ViewRenderer;
-use Yiisoft\View\WebView;
-use Yiisoft\Csrf\CsrfToken;
 use Yiisoft\Swagger\Tests\Mock\MockCsrfTokenStorage;
+use Yiisoft\View\WebView;
 use Yiisoft\Yii\View\CsrfViewInjection;
+use Yiisoft\Yii\View\ViewRenderer;
 
 final class SwaggerUiTest extends TestCase
 {
-    public function testSwaggerUiMiddleware(): void
+    public function testSwaggerUiMiddlewareWithUrl(): void
     {
         $middleware = $this->createMiddleware();
-
         $this->assertNotSame($middleware, $middleware->withJsonUrl('/'));
+    }
 
-        $response = $middleware
-            ->process($this->createServerRequest(), $this->createRequestHandler());
+    public function testSwaggerUiMiddleware(): void
+    {
+        $middleware = $this->createMiddleware()->withJsonUrl('/');
+
+        $request = $this->createServerRequest();
+        $handler = $this->createRequestHandler();
+
+        $response = $middleware->process($request, $handler);
 
         $this->assertEquals(200, $response->getStatusCode());
     }
@@ -54,6 +54,18 @@ final class SwaggerUiTest extends TestCase
             CacheInterface::class => new ArrayCache(),
             DataResponseFactoryInterface::class => DataResponseFactory::class,
             ResponseFactoryInterface::class => Psr17Factory::class,
+            ViewRenderer::class => function (Container $container) {
+                return new ViewRenderer(
+                    $container->get(DataResponseFactoryInterface::class),
+                    $container->get(Aliases::class),
+                    $this->createMock(WebView::class),
+
+                    $this->getCsrfViewInjection(),
+                    __DIR__,
+                    '',
+                    []
+                );
+            }
         ];
 
         return new Container($definitions);
@@ -63,15 +75,6 @@ final class SwaggerUiTest extends TestCase
     {
         $container = $this->createContainer();
 
-        $viewRenderer = new ViewRenderer(
-            $container->get(DataResponseFactoryInterface::class),
-            $container->get(Aliases::class),
-            $this->createMock(WebView::class),
-            $this->getCsrfViewInjection(),
-            __DIR__,
-            '',
-            []
-        );
 
         return new SwaggerUi(
             $container->get(ViewRenderer::class),
@@ -79,7 +82,8 @@ final class SwaggerUiTest extends TestCase
         );
     }
 
-    private function getCsrfViewInjection(): CsrfViewInjection {
+    private function getCsrfViewInjection(): CsrfViewInjection
+    {
         $csrfToken = $this->createCsrfToken();
 
         return new CsrfViewInjection($csrfToken);
