86f8f9a88e052a2e22d829852a491c973aad75b6
Saint <saint@watson.local>
2020-10-01 10:56:08 +0300

    Add test for SwaggerService

--- tests/SwaggerServiceTest.php
+++ tests/SwaggerServiceTest.php
@@ -4,75 +4,45 @@ declare(strict_types=1);
 
 namespace Yiisoft\Swagger\Tests;
 
-use Nyholm\Psr7\Factory\Psr17Factory;
-use Nyholm\Psr7\Response;
-use Nyholm\Psr7\ServerRequest;
 use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerInterface;
-use Psr\Http\Message\ResponseFactoryInterface;
-use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Server\RequestHandlerInterface;
-use Psr\SimpleCache\CacheInterface;
 use Yiisoft\Aliases\Aliases;
-use Yiisoft\Cache\ArrayCache;
-use Yiisoft\DataResponse\DataResponseFactory;
-use Yiisoft\DataResponse\DataResponseFactoryInterface;
 use Yiisoft\Di\Container;
-use Yiisoft\Http\Method;
-use Yiisoft\Swagger\Middleware\SwaggerJson;
+use Yiisoft\Swagger\Exception\InvalidArgumentException;
 use Yiisoft\Swagger\Service\SwaggerService;
 
-final class SwaggerJsonTest extends TestCase
+final class SwaggerServiceTest extends TestCase
 {
-    public function testSwaggerJsonMiddleware(): void
+    public function testSwaggerServiceView(): void
     {
-        $middleware = $this->createMiddleware();
+        $service = $this->createService();
+        $viewPath = $service->getViewPath();
+        $viewFile = $viewPath . '/' . $service->getViewName() . '.php';
 
-        $this->assertNotSame($middleware, $middleware->withAnnotationPaths([]));
-        $this->assertNotSame($middleware, $middleware->withCache());
-
-        $response = $middleware
-            ->withAnnotationPaths([__DIR__ . '/Mock'])
-            ->process($this->createServerRequest(), $this->createRequestHandler());
-
-        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertDirectoryExists($service->getViewPath());
+        $this->assertFileExists($viewFile);
     }
 
-    private function createMiddleware(): SwaggerJson
+    public function testSwaggerServiceEmptyArrayFetch(): void
+    {
+        $service = $this->createService();
+        $this->expectException(InvalidArgumentException::class);
+        $service->fetch([]);
+    }
+
+    private function createService(): SwaggerService
     {
         $container = $this->createContainer();
 
-        return new SwaggerJson(
-            $container->get(CacheInterface::class),
-            $container->get(DataResponseFactoryInterface::class),
-            $container->get(SwaggerService::class)
-        );
+        return new SwaggerService($container);
     }
 
     private function createContainer(): ContainerInterface
     {
         $definitions = [
-            Aliases::class => new Aliases(),
-            CacheInterface::class => new ArrayCache(),
-            DataResponseFactoryInterface::class => DataResponseFactory::class,
-            ResponseFactoryInterface::class => Psr17Factory::class,
+            Aliases::class => new Aliases()
         ];
 
         return new Container($definitions);
-    }
-
-    private function createServerRequest(string $method = Method::GET, $headers = []): ServerRequestInterface
-    {
-        return new ServerRequest($method, '/', $headers);
-    }
-
-    private function createRequestHandler(): RequestHandlerInterface
-    {
-        $requestHandler = $this->createMock(RequestHandlerInterface::class);
-        $requestHandler
-            ->method('handle')
-            ->willReturn(new Response(200));
-
-        return $requestHandler;
     }
 }
