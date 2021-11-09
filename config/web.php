12cff3fa65d1b815124dd88a63c676c5c376d922
Alexey Chernousov <iamsaint@yandex.ru>
2021-11-09 14:43:49 +0300

    Add definition for SwaggerService

--- config/web.php
+++ config/web.php
@@ -3,8 +3,10 @@
 declare(strict_types=1);
 
 use Yiisoft\Swagger\Middleware\SwaggerUI;
+use Yiisoft\Swagger\Service\SwaggerService;
 
 return [
+    SwaggerService:class => SwaggerService:class,
     SwaggerUI::class => [
         '__construct()' => [
             'params' => $params['yiisoft/yii-swagger']['ui-params'],
