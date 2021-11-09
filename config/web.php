1aaddb1b23e6e9570c95e18c70359e606d0c68d2
Alexey Chernousov <iamsaint@yandex.ru>
2021-11-09 14:58:00 +0300

    Fix class name

--- config/web.php
+++ config/web.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-use Yiisoft\Swagger\Middleware\SwaggerUI;
+use Yiisoft\Swagger\Middleware\SwaggerUi;
 use Yiisoft\Swagger\Service\SwaggerService;
 
 return [
     SwaggerService::class => SwaggerService::class,
-    SwaggerUI::class => [
+    SwaggerUi::class => [
         '__construct()' => [
             'params' => $params['yiisoft/yii-swagger']['ui-params'],
         ],
