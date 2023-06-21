b78b2449ae46d21d28714561d36aff8d2a8a8e56
Dmitrii Derepko <xepozz@list.ru>
2023-06-21 19:35:16 +0300

    Revert "Convert middleware to request handler"
    This reverts commit 44e1fb1e47bcf807dfe9391a5bf74e0c79a07273.

--- src/Middleware/SwaggerJson.php
+++ src/Middleware/SwaggerJson.php
@@ -8,12 +8,13 @@ use DateInterval;
 use OpenApi\Annotations\OpenApi;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Yiisoft\Cache\CacheInterface;
 use Yiisoft\DataResponse\DataResponseFactoryInterface;
 use Yiisoft\Swagger\Service\SwaggerService;
 
-final class SwaggerJson implements RequestHandlerInterface
+final class SwaggerJson implements MiddlewareInterface
 {
     private array $annotationPaths = [];
     private bool $enableCache = false;
@@ -26,7 +27,7 @@ final class SwaggerJson implements RequestHandlerInterface
     ) {
     }
 
-    public function handle(ServerRequestInterface $request): ResponseInterface
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         /** @var OpenApi $openApi */
         $openApi = !$this->enableCache ? $this->swaggerService->fetch($this->annotationPaths) : $this->cache->getOrSet(
