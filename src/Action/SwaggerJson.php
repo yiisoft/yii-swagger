135a5e28566cb231afc275e8fc312b3b66f0a81c
Dmitrii Derepko <xepozz@list.ru>
2023-06-21 19:44:53 +0300

    Copy SwaggerJson and make it a request handler instead of a middleware

---
+++ src/Action/SwaggerJson.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Swagger\Action;
+
+use DateInterval;
+use OpenApi\Annotations\OpenApi;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Yiisoft\Cache\CacheInterface;
+use Yiisoft\DataResponse\DataResponseFactoryInterface;
+use Yiisoft\Swagger\Service\SwaggerService;
+
+final class SwaggerJson implements RequestHandlerInterface
+{
+    private array $annotationPaths = [];
+    private bool $enableCache = false;
+    private DateInterval|int|null $cacheTTL = null;
+
+    public function __construct(
+        private CacheInterface $cache,
+        private DataResponseFactoryInterface $responseFactory,
+        private SwaggerService $swaggerService
+    ) {
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        if (!$this->enableCache) {
+            $openApi = $this->swaggerService->fetch($this->annotationPaths);
+        } else {
+            /** @var OpenApi $openApi */
+            $openApi = $this->cache->getOrSet(
+                [self::class, $this->annotationPaths],
+                fn () => $this->swaggerService->fetch($this->annotationPaths),
+                $this->cacheTTL,
+            );
+        }
+
+        return $this->responseFactory->createResponse($openApi);
+    }
+
+    public function withAnnotationPaths(string ...$annotationPaths): self
+    {
+        $new = clone $this;
+        $new->annotationPaths = $annotationPaths;
+        return $new;
+    }
+
+    /**
+     * @param DateInterval|int|null $cacheTTL
+     */
+    public function withCache(DateInterval|int $cacheTTL = null): self
+    {
+        $new = clone $this;
+        $new->enableCache = true;
+        $new->cacheTTL = $cacheTTL;
+        return $new;
+    }
+}
