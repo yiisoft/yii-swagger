ad4ac083d0e2179ea4bee363b8a108692541783d
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-01-26 18:46:02 +0300

    Add support `yiisoft/assets` version of `^5.0` (#109)
    Co-authored-by: Alexander Makarov <sam@rmcreative.ru>

--- src/Asset/SwaggerUiAsset.php
+++ src/Asset/SwaggerUiAsset.php
@@ -5,16 +5,28 @@ declare(strict_types=1);
 namespace Yiisoft\Swagger\Asset;
 
 use Yiisoft\Assets\AssetBundle;
+use Yiisoft\Assets\AssetManager;
 
+/**
+ * @psalm-import-type CssFile from AssetManager
+ * @psalm-import-type JsFile from AssetManager
+ */
 final class SwaggerUiAsset extends AssetBundle
 {
     public ?string $basePath = '@assets';
 
     public ?string $baseUrl = '@assetsUrl';
 
+    /**
+     * @psalm-var array<string|CssFile>
+     */
     public array $css = [
         'swagger-ui.css',
     ];
+
+    /**
+     * @psalm-var array<string|JsFile>
+     */
     public array $js = [
         'swagger-ui-bundle.js',
         'swagger-ui-standalone-preset.js',
