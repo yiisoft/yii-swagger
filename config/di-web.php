1e6fb2643de1d87646c6d0131e15f0ecd268dfb1
Rustam <rmamdaminov@gmail.com>
2025-12-03 11:59:18 +0500

    Remove deprecated middlewares

--- config/di-web.php
+++ config/di-web.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 use Yiisoft\Cache\CacheInterface;
 use Yiisoft\DataResponse\DataResponseFactoryInterface;
-use Yiisoft\Yii\Swagger\Middleware\SwaggerJson as OldSwaggerJson;
-use Yiisoft\Yii\Swagger\Middleware\SwaggerUi as OldSwaggerUi;
 use Yiisoft\Yii\Swagger\Action\SwaggerJson;
 use Yiisoft\Yii\Swagger\Action\SwaggerUi;
 use Yiisoft\Yii\Swagger\Service\SwaggerService;
@@ -18,32 +16,11 @@ return [
             $params['yiisoft/yii-swagger']['options'],
         ],
     ],
-
-    OldSwaggerUi::class => [
-        '__construct()' => [
-            'params' => $params['yiisoft/yii-swagger']['ui-params'],
-        ],
-    ],
     SwaggerUi::class => [
         '__construct()' => [
             'params' => $params['yiisoft/yii-swagger']['ui-params'],
         ],
     ],
-
-    OldSwaggerJson::class => static function (
-        CacheInterface $cache,
-        DataResponseFactoryInterface $responseFactory,
-        SwaggerService $swaggerService,
-    ) use ($params) {
-        $params = $params['yiisoft/yii-swagger'];
-        $swaggerJson = new OldSwaggerJson($cache, $responseFactory, $swaggerService);
-
-        if (array_key_exists('cacheTTL', $params)) {
-            $swaggerJson = $swaggerJson->withCache($params['cacheTTL']);
-        }
-
-        return $swaggerJson->withAnnotationPaths(...$params['source-paths']);
-    },
     SwaggerJson::class => static function (
         CacheInterface $cache,
         DataResponseFactoryInterface $responseFactory,
